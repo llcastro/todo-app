efe1316ccf8589956fcd28c9db1a968ce09796a3
Luis Castro <langelolcastro@gmail.com>
2020-04-16 21:09:10 -0300

    add authorization to todo itens

--- backend/Controllers/TodoItemsController.cs
+++ backend/Controllers/TodoItemsController.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using todo_app.Models;
 
 namespace todo_app.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class TodoItemsController : ControllerBase
--- backend/Models/TodoItem.cs
+++ backend/Models/TodoItem.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace todo_app.Models
 {
     public class TodoItem
     {
+        [Key]
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
